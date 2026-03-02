acb57017ba81302020866ef67869cb8c2ba7c91b
Hellen <hyanesd@eafit.edu.co>
2026-03-01 19:47:17 -0500

    Tutorial 3 completed

--- pages/apps.py
+++ pages/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.utils.module_loading import import_string
+from django.conf import settings
 
 
 class PagesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages'
+
+    def ready(self):
+        # Carga dinámicamente la clase definida en settings
+        ImageStorageClass = import_string(settings.IMAGE_STORAGE_CLASS)
--- pages/urls.py
+++ pages/urls.py
@@ -6,7 +6,12 @@ from .views import (
     ProductIndexView,
     ProductShowView,
     ProductCreateView,
+    CartView,
+    CartRemoveAllView,
 )
+from .views import ImageViewFactory
+from .utils import ImageLocalStorage
+from .views import ImageViewNoDI
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -16,4 +21,17 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='products'),
     path('products/create/', ProductCreateView.as_view(), name='products.create'),
     path('products/<str:id>/', ProductShowView.as_view(), name='products.show'),
+
+    # --- CART ROUTES ---
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>/', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll/', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
+        # --- IMAGE DI ROUTES ---
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+
+    # --- IMAGE NO DI ---
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('imagenotdi/save/', ImageViewNoDI.as_view(), name='imagenodi_save'),
 ]
